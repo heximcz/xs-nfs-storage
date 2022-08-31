08970ea501e01babc024dfc994c5b30e64164faa
hexim <spacek@best-net.cz>
2022-08-31 23:16:56 +0200

    comment unused

--- src/XApi/XApiWrapper.py
+++ src/XApi/XApiWrapper.py
@@ -8,7 +8,10 @@ from src.MySQL import MySQL
 
 class XApiWrapper:
     """
-    funkce
+    Check all disk od all NFS SRs
+    Put actual version to database
+
+    Just run()
     """
     
     def __init__(self, config: LoadConfig) -> None:
@@ -16,34 +19,34 @@ class XApiWrapper:
         self.__mysql = MySQL(config)
         self.__xapi = XApiConnect(config)
     
-    def update_sr(self):
-        """
-        Add new NFS SR and update changes in name-label and name-description.
-        :return: None
-        """
+    # def update_sr(self):
+    #     """
+    #     Add new NFS SR and update changes in name-label and name-description.
+    #     :return: None
+    #     """
 
-        # load NFS SRs from xapi
-        sr = XApiStorageRepositories(self.__config, self.__xapi)
-        xapi_all_nfs_sr: list[XApiOneStorage] = sr.get_Storages()
-        # compare xapi and mysql SRs by uuid, add new or update changes
-        for xapi_sr in xapi_all_nfs_sr:
-            sr_data = self.__mysql.get_sr_by_uuid(xapi_sr.sr_uuid)
-            if sr_data is None:
-                # pridej nove SR do DB
-                self.__mysql.add_new_sr(
-                    xapi_sr.sr_uuid,
-                    xapi_sr.sr_name_label,
-                    xapi_sr.sr_name_description
-                    )
-            else:
-                # aktualizuj hodnoty (muze se zmenit name label nebo description) v pripade zmeny
-                if xapi_sr.sr_name_label != sr_data[2] or xapi_sr.sr_name_description != sr_data[3]:
-                    self.__mysql.update_sr(
-                        xapi_sr.sr_uuid, 
-                        xapi_sr.sr_name_label, 
-                        xapi_sr.sr_name_description
-                        )
-        self.__config.logger.info("SR_List - Updated.")
+    #     # load NFS SRs from xapi
+    #     sr = XApiStorageRepositories(self.__config, self.__xapi)
+    #     xapi_all_nfs_sr: list[XApiOneStorage] = sr.get_Storages()
+    #     # compare xapi and mysql SRs by uuid, add new or update changes
+    #     for xapi_sr in xapi_all_nfs_sr:
+    #         sr_data = self.__mysql.get_sr_by_uuid(xapi_sr.sr_uuid)
+    #         if sr_data is None:
+    #             # pridej nove SR do DB
+    #             self.__mysql.add_new_sr(
+    #                 xapi_sr.sr_uuid,
+    #                 xapi_sr.sr_name_label,
+    #                 xapi_sr.sr_name_description
+    #                 )
+    #         else:
+    #             # aktualizuj hodnoty (muze se zmenit name label nebo description) v pripade zmeny
+    #             if xapi_sr.sr_name_label != sr_data[2] or xapi_sr.sr_name_description != sr_data[3]:
+    #                 self.__mysql.update_sr(
+    #                     xapi_sr.sr_uuid, 
+    #                     xapi_sr.sr_name_label, 
+    #                     xapi_sr.sr_name_description
+    #                     )
+    #     self.__config.logger.info("SR_List - Updated.")
 
     def run(self):
         """
