a5e5d8fe672307f41da0bb12b31a8583a6667a8c
hexim <spacek@best-net.cz>
2022-09-01 23:07:41 +0200

    comment unused

--- src/MySQL/MySQL.py
+++ src/MySQL/MySQL.py
@@ -13,7 +13,7 @@ class MySQL:
         """
         self.__mysql = config.get_mysql()
         try:
-            self.__mydb = mysql.connector.connect(
+            self._mydb = mysql.connector.connect(
                 host=self.__mysql['host'],
                 user=self.__mysql['user'],
                 passwd=self.__mysql['passwd'],
@@ -39,43 +39,43 @@ class MySQL:
     #         """)
     #     return my_cursor.fetchall()
     
-    def get_sr_by_uuid(self, uuid: str) -> list | None:
-        """
-        Get one SR by UUID
-        :return: list
-        """
-        my_cursor = self.__mydb.cursor()
-        my_cursor.execute("""
-            SELECT * FROM `sr_list`
-            WHERE `sr_uuid` LIKE '""" + uuid + """'
-            """)
-        return my_cursor.fetchone()
+    # def get_sr_by_uuid(self, uuid: str) -> list | None:
+    #     """
+    #     Get one SR by UUID
+    #     :return: list
+    #     """
+    #     my_cursor = self.__mydb.cursor()
+    #     my_cursor.execute("""
+    #         SELECT * FROM `sr_list`
+    #         WHERE `sr_uuid` LIKE '""" + uuid + """'
+    #         """)
+    #     return my_cursor.fetchone()
 
-    def add_new_sr(self, sr_uuid: str, name_label: str, name_description: str) -> None:
-        """
-        Add new SR
-        :return: None
-        """
-        my_cursor = self.__mydb.cursor()
-        my_cursor.execute("""
-            INSERT INTO `sr_list`
-            (`uid`, `sr_uuid`, `name_label`, `name_description`, `created`)
-            VALUES
-            (NULL, '""" + sr_uuid + """', '""" + name_label + """', '""" + name_description + """', NOW());
-            """)
-        self.__mydb.commit()
-        my_cursor.close()
+    # def add_new_sr(self, sr_uuid: str, name_label: str, name_description: str) -> None:
+    #     """
+    #     Add new SR
+    #     :return: None
+    #     """
+    #     my_cursor = self.__mydb.cursor()
+    #     my_cursor.execute("""
+    #         INSERT INTO `sr_list`
+    #         (`uid`, `sr_uuid`, `name_label`, `name_description`, `created`)
+    #         VALUES
+    #         (NULL, '""" + sr_uuid + """', '""" + name_label + """', '""" + name_description + """', NOW());
+    #         """)
+    #     self.__mydb.commit()
+    #     my_cursor.close()
 
-    def update_sr(self, sr_uuid: str, name_label: str, name_description: str) -> None:
-        """
-        Update existing SR
-        :return: None
-        """
-        my_cursor = self.__mydb.cursor()
-        my_cursor.execute("""
-            UPDATE `sr_list`
-            SET `name_label` = '""" + name_label + """' , `name_description` = '""" + name_description + """'
-            WHERE `sr_uuid` = '""" + sr_uuid + """'
-            """)
-        self.__mydb.commit()
-        my_cursor.close()
+    # def update_sr(self, sr_uuid: str, name_label: str, name_description: str) -> None:
+    #     """
+    #     Update existing SR
+    #     :return: None
+    #     """
+    #     my_cursor = self.__mydb.cursor()
+    #     my_cursor.execute("""
+    #         UPDATE `sr_list`
+    #         SET `name_label` = '""" + name_label + """' , `name_description` = '""" + name_description + """'
+    #         WHERE `sr_uuid` = '""" + sr_uuid + """'
+    #         """)
+    #     self.__mydb.commit()
+    #     my_cursor.close()
