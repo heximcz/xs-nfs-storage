be6ddb8184837769d33ff735017e565c65ece7e6
hexim <spacek@best-net.cz>
2022-09-02 02:23:22 +0200

    fix commnts

--- src/XApi/XApiWrapper.py
+++ src/XApi/XApiWrapper.py
@@ -22,8 +22,10 @@ class XApiWrapper:
     
     def run(self):
         """
-        Add actual running version of vdi (SR and VM) to database
+        Add actual running version of VDIs (SRs and VMs) to database
         """
+
+        # generate lists from xapi
         try:
             self.__xapi.open()
 
@@ -50,37 +52,22 @@ class XApiWrapper:
         finally:
             self.__xapi.close()
 
+        # save new generated version to db
 
-        ### all to db ###
-
-        # Create new version id
+        # create new version id
         db = XApiMysql(self.__config)
         version_id = db.create_new_version()
 
-        # all virtual machines
+        # get all virtual machines list
         vms = vm.get_VMs()
         for one_vm in vms:
-            # VM uuid: 0e16dc5f-a169-c322-2790-fa4e3ca3e47f | 
-            # VM name_label: Deb11 shapshot on SR01 | 
-            # VM is_a_snapshot: True | 
-            # 
-            # VBD(object):
-            # VBD uuid: 2b24c5d5-e0d4-52ed-1c3e-6bf367b1381a | 
-            # VBD device: xvda | 
-            # 
-            # VDI(object): 
-            # VDI uuid: 71e5b355-e09d-435c-ade0-f052ddf7df5f | 
-            # VDI is_a_snapshot: True | 
-            # VDI name_label: Debian 11x2 on ZFS SR01 0 | 
-            # 
-            # SR(object): 
-            # SR uuid: 7590b1d2-521a-2ccb-92e8-f1192b18a76c, 
-            # SR name_label: STORAGE 01, 
-            # SR name_description: NFS SR [172.44.1.5:/xcpng/xenserver]
-
-            # 1. add SR
-            # 2. add VM
-            # 3. add VDI
+            # add SR to db
             sr_id = db.add_sr(one_vm.vbd.vdi.sr, version_id)
+            # add VM to db
             vm_id = db.add_vm(one_vm, version_id)
+            # add VDI to db
             db.add_vdi(one_vm.vbd.vdi, version_id, sr_id, vm_id, one_vm.vbd.vbd_device)
+
+        # TODO add try/except to all to db
+        # TODO add delete method - delete x-days older
+        # TODO add print reconstruction output from db
